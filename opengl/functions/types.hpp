c7eac61df5e96c6d30ecb5f9cea1f9f3a847e66b
iamdramaticleo <iamdramaticleo@gmail.com>
2025-09-30 20:52:59 +0300

    Add OpenGL module with function initialization
    Introduced a new OpenGL module in the graphics system, including core functions, buffer handling, and command constants. Integrated it into the main project with function initialization and rendering setup in `main.cpp`. Updated CMake files to configure the module and its dependencies.

---
+++ opengl/functions/types.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+
+namespace opengl::functions
+{
+    using GLbitfield = uint32_t;
+    using GLuint     = uint32_t;
+
+    using GLsizei    =  int32_t;
+
+    using GLfloat    =  float;
+}
