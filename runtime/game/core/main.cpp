63e4fd176d96156e0c24595a3188b48401a64b68
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-10 16:18:08 +0200

    Refactor image handling to remove `core::data::image`.
    Replaced `core::data::image` with `core::image` and refactored related methods to use width, height, and buffer parameters directly. Simplifies dependencies and improves modularity in image processing and OpenGL texture handling.

--- runtime/game/core/main.cpp
+++ runtime/game/core/main.cpp
@@ -157,8 +157,8 @@ auto main() -> int32_t
 
     opengl::Texture base_texture { opengl::constants::texture_2d };
     base_texture.create();
-    base_texture.storage(base_image, opengl::constants::rgb8, 1);
-    base_texture.upload (base_image, opengl::constants::rgb,  0);
+    base_texture.storage(base_image.width, base_image.height, opengl::constants::rgb8, 1);
+    base_texture.upload (base_image.width, base_image.height, opengl::constants::rgb,  0, core::as_bytes(base_image.pixels));
 
     opengl::Buffer transform_ubo;
     transform_ubo.create();
