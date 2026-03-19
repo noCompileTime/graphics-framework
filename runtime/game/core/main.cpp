0d34333e111079bbccc6951dfac991117cd4d037
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-19 12:44:37 +0200

    Rename `polygon_mode` to `polygon` for consistency
    Refactored the `PipelineDebug::polygon_mode` method and its references to `polygon` for improved naming consistency. Added `<span>` to headers for potential use in future updates.

--- runtime/game/core/main.cpp
+++ runtime/game/core/main.cpp
@@ -232,12 +232,12 @@ auto main() -> int32_t
     {
         if (wireframe_mode)
         {
-            opengl::PipelineDebug::polygon_mode(opengl::constants::fill_mode);
+            opengl::PipelineDebug::polygon(opengl::constants::fill_mode);
             //opengl::Pipeline::enable(opengl::constants::cull_test);
         }
         else
         {
-            opengl::PipelineDebug::polygon_mode(opengl::constants::line_mode);
+            opengl::PipelineDebug::polygon(opengl::constants::line_mode);
             //opengl::Pipeline::disable(opengl::constants::cull_test);
         }
 
