b43a9e6081b82f9ff076a25384155982f9bb9a71
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-12 14:29:57 +0300

    Add debug shaders and axis/bounding box rendering support
    Introduced new vertex and fragment shaders for debugging and updated the rendering pipeline to support axis and bounding box visualization. Enhanced `core::vertex::type` with an `editor` type and added utilities in `Primitives` for generating debug geometry. Updated camera and rendering logic to integrate these changes.

--- runtime/game/core/main.cpp
+++ runtime/game/core/main.cpp
@@ -1,4 +1,5 @@
 #include "core/file.hpp"
+#include "core/primitives.hpp"
 #include "core/time.hpp"
 #include "core/utility.hpp"
 
@@ -79,6 +80,14 @@ auto main() -> std::int32_t
     base_shader_fragment.create();
     base_shader_fragment.source(core::File::read("shaders/base_shader.frag"));
 
+    opengl::ShaderStage debug_shader_vertex { opengl::constants::vertex_shader };
+    debug_shader_vertex.create();
+    debug_shader_vertex.source(core::File::read("shaders/base_debug_shader.vert"));
+
+    opengl::ShaderStage debug_shader_fragment { opengl::constants::fragment_shader };
+    debug_shader_fragment.create();
+    debug_shader_fragment.source(core::File::read("shaders/base_debug_shader.frag"));
+
     opengl::ShaderStage sprite_shader_vertex { opengl::constants::vertex_shader };
     sprite_shader_vertex.create();
     sprite_shader_vertex.source(core::File::read("shaders/base_sprite_shader.vert"));
@@ -100,6 +109,12 @@ auto main() -> std::int32_t
     base_shader.attach(base_shader_vertex);
     base_shader.attach(base_shader_fragment);
     base_shader.link();
+
+    opengl::Shader debug_shader;
+    debug_shader.create();
+    debug_shader.attach(debug_shader_vertex);
+    debug_shader.attach(debug_shader_fragment);
+    debug_shader.link();
 
     opengl::Shader sprite_shader;
     sprite_shader.create();
@@ -152,6 +167,42 @@ auto main() -> std::int32_t
     ground_vao.attach({ 1, 2, opengl::constants::float_type, offsetof(core::vertex::type::model, texcoord.x) });
     ground_vao.attach({ 2, 3, opengl::constants::float_type, offsetof(core::vertex::type::model,   normal.x) });
 
+    auto [axis_vertices, axis_elements] = core::Primitives::create_axis({ 10.0f, 10.0f, 10.0f });
+
+    opengl::Buffer axis_vbo;
+    axis_vbo.create();
+    axis_vbo.storage(core::as_bytes(axis_vertices), opengl::constants::static_draw);
+
+    opengl::Buffer axis_ebo;
+    axis_ebo.create();
+    axis_ebo.storage(core::as_bytes(axis_elements), opengl::constants::static_draw);
+
+    opengl::VertexArray axis_vao;
+    axis_vao.create();
+    axis_vao.attach_vertices(axis_vbo, sizeof(core::vertex::type::editor));
+    axis_vao.attach_elements(axis_ebo);
+
+    axis_vao.attach({ 0, 3, opengl::constants::float_type, offsetof(core::vertex::type::editor, position.x) });
+    axis_vao.attach({ 1, 3, opengl::constants::float_type, offsetof(core::vertex::type::editor, extra.x) });
+
+    auto [debug_vertices, debug_elements] = core::Primitives::create_bouding_box({ 1.0f, 1.0f, 1.0f }, { 1.0f, 1.0f, 1.0f });
+
+    opengl::Buffer debug_vbo;
+    debug_vbo.create();
+    debug_vbo.storage(core::as_bytes(debug_vertices), opengl::constants::static_draw);
+
+    opengl::Buffer debug_ebo;
+    debug_ebo.create();
+    debug_ebo.storage(core::as_bytes(debug_elements), opengl::constants::static_draw);
+
+    opengl::VertexArray debug_vao;
+    debug_vao.create();
+    debug_vao.attach_vertices(debug_vbo, sizeof(core::vertex::type::editor));
+    debug_vao.attach_elements(debug_ebo);
+
+    debug_vao.attach({ 0, 3, opengl::constants::float_type, offsetof(core::vertex::type::editor, position.x) });
+    debug_vao.attach({ 1, 3, opengl::constants::float_type, offsetof(core::vertex::type::editor, extra.x) });
+
     opengl::Sampler base_sampler;
     base_sampler.create();
     base_sampler.parameter(opengl::constants::min_filter, opengl::constants::nearest);
@@ -164,9 +215,18 @@ auto main() -> std::int32_t
     base_texture.storage(base_image.width, base_image.height, opengl::constants::rgb8, 1);
     base_texture.upload(base_image.width, base_image.height, opengl::constants::rgb, 0, opengl::constants::unsigned_byte, base_image.pixels);
 
+    math::quat x_view_rotation;
+    math::quat y_view_rotation;
+
+    x_view_rotation.rotation({ 1.0f, 0.0f, 0.0f }, math::radians( 45.0f));
+    y_view_rotation.rotation({ 0.0f, 1.0f, 0.0f }, math::radians(-45.0f));
+
+    auto view_matrix = (x_view_rotation * y_view_rotation).matrix();
+         view_matrix.translation({ 0.0f, 0.0f, -5.0f });
+
     core::data::camera camera_data;
+    camera_data.view = view_matrix;
     camera_data.projection.perspective(math::radians(45.0f), static_cast<float>(window_width) / static_cast<float>(window_height), 0.1f, 100.0f);
-    camera_data.view.translation({ 0.0f, -0.25f, -3.5f });
 
     opengl::Buffer camera_ubo;
     camera_ubo.create();
@@ -257,13 +317,23 @@ auto main() -> std::int32_t
 
         transform_ubo.upload(core::as_bytes(object.matrix()), 0);
 
-        opengl::Commands::draw_elements(opengl::constants::triangles, cube_elements.size(), opengl::constants::unsigned_int, 0);
+        //opengl::Commands::draw_elements(opengl::constants::triangles, cube_elements.size(), opengl::constants::unsigned_int, 0);
 
            ground_vao.bind();
 
         transform_ubo.upload(core::as_bytes(ground_transform), 0);
 
-        opengl::Commands::draw_elements(opengl::constants::triangles, ground_elements.size(), opengl::constants::unsigned_int, 0);
+        //opengl::Commands::draw_elements(opengl::constants::triangles, ground_elements.size(), opengl::constants::unsigned_int, 0);
+
+        debug_shader.bind();
+
+        axis_vao.bind();
+
+        opengl::Commands::draw_elements(opengl::constants::lines, axis_elements.size(), opengl::constants::unsigned_int, 0);
+
+        debug_vao.bind();
+
+        opengl::Commands::draw_elements(opengl::constants::lines, debug_elements.size(), opengl::constants::unsigned_int, 0);
 
         window_manager.context().update();
     }
