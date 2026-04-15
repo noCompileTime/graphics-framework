2f62a8bd302e5dabdbcb14b8eb75ea3a7a64760c
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-15 22:35:35 +0300

    Update clear color for OpenGL commands

--- runtime/game/core/main.cpp
+++ runtime/game/core/main.cpp
@@ -309,7 +309,7 @@ auto main() -> std::int32_t
 
                 object.update();
 
-        opengl::Commands::clear(0.105f, 0.235f, 0.325f, 1.0f);
+        opengl::Commands::clear(0.2745f, 0.5176f, 0.1961f, 1.0f);
         opengl::Commands::clear(opengl::constants::color_buffer | opengl::constants::depth_buffer);
 
          model_shader.bind();
