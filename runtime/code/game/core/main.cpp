01907eaf806a20a0445ff38defe3f20803142521
iamdramaticleo <iamdramaticleo@gmail.com>
2026-06-11 16:23:04 +0300

    Simplify matrix inversion calls by removing redundant `math::` namespace prefixes.

--- runtime/code/game/core/main.cpp
+++ runtime/code/game/core/main.cpp
@@ -58,7 +58,7 @@ auto ray_to_world(const math::vec2& point, const int32_t window_width, const int
         1.0f - 2.0f * point.y / static_cast<float>(window_height)
     };
 
-    const auto inverse_matrix = math::inverse(camera.projection * camera.view);
+    const auto inverse_matrix = inverse(camera.projection * camera.view);
 
     auto origin  = inverse_matrix * math::vec4 { ndc.x, ndc.y, -1.0f, 1.0f };
     auto finish  = inverse_matrix * math::vec4 { ndc.x, ndc.y,  1.0f, 1.0f };
@@ -365,7 +365,7 @@ auto main() -> int32_t
          view_matrix.translate(camera_position);
 
     core::data::camera camera_data; // TODO rename this with scene_ or base_ or even game_
-    camera_data.view = math::inverse_rigid(view_matrix);
+    camera_data.view = inverse_rigid(view_matrix);
     camera_data.projection.perspective(math::radians(45.0f), view_width / view_height, 0.1f, 100.0f);
 
     core::data::camera view_camera_data;
